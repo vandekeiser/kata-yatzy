5b4db624c891b7a11f4f46413bf582937ae5529c
Laurent Claisse <you@example.com>
2019-04-16 22:44:23 +0200

    test legibility

--- src/test/java/fr/cla/kata/yatzy/RollTest.java
+++ src/test/java/fr/cla/kata/yatzy/RollTest.java
@@ -5,71 +5,134 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RollTest {
 
     @Test
     public void sumOfAllDice() {
-        assertEquals(15, Roll.roll(2,3,4,5,1).sumOfAllDice());
+        assertThat(
+            Roll.roll(2,3,4,5,1).sumOfAllDice()
+        ).isEqualTo(15);
     }
 
     @Test
     public void allDiceHaveTheSameNumber() {
-        assertThat(Roll.roll(1,1,1,1,1).allDiceHaveTheSameNumber()).isTrue();
-        assertThat(Roll.roll(1,1,2,1,1).allDiceHaveTheSameNumber()).isFalse();
+        assertThat(
+            Roll.roll(1,1,1,1,1).allDiceHaveTheSameNumber()
+        ).isTrue();
+
+        assertThat(
+            Roll.roll(1,1,2,1,1).allDiceHaveTheSameNumber()
+        ).isFalse();
     }
 
     @Test
     public void count() {
-        assertThat(Roll.roll(1,1,2,1,1).count(1)).isEqualTo(4);
-        assertThat(Roll.roll(1,3,2,1,5).count(1)).isEqualTo(2);
-        assertThat(Roll.roll(4,3,2,4,5).count(1)).isEqualTo(0);
+        assertThat(
+            Roll.roll(1,1,2,1,1).count(1)
+        ).isEqualTo(4);
+
+        assertThat(
+            Roll.roll(1,3,2,1,5).count(1)
+        ).isEqualTo(2);
+
+        assertThat(
+            Roll.roll(4,3,2,4,5).count(1)
+        ).isEqualTo(0);
     }
 
     @Test
     public void highestPair() {
-        assertThat(Roll.roll(1,1,2,5,2).highestPair()).isEqualTo(Optional.of(new Pair(2)));
-        assertThat(Roll.roll(1,2,3,4,5).highestPair()).isEmpty();
+        assertThat(
+            Roll.roll(1,1,2,5,2).highestPair()
+        ).isEqualTo(
+            Optional.of(new Pair(2))
+        );
+
+        assertThat(
+            Roll.roll(1,2,3,4,5).highestPair()
+        ).isEmpty();
     }
 
     @Test
     public void twoPairs() {
-        assertThat(Roll.roll(1,1,3,5,3).twoPairs()).isEqualTo(Optional.of(new TwoPairs(1, 3)));
-        assertThat(Roll.roll(1,1,3,5,4).twoPairs()).isEmpty();
-        assertThat(Roll.roll(1,2,3,4,5).twoPairs()).isEmpty();
+        assertThat(
+            Roll.roll(1,1,3,5,3).twoPairs()
+        ).isEqualTo(Optional.of(new TwoPairs(1, 3)));
+
+        assertThat(
+            Roll.roll(1,1,3,5,4).twoPairs()
+        ).isEmpty();
+
+        assertThat(
+            Roll.roll(1,2,3,4,5).twoPairs()
+        ).isEmpty();
     }
 
     @Test
     public void threeOfAKind() {
-        assertThat(Roll.roll(1,1,3,1,3).threeOfAKind()).isEqualTo(Optional.of(new ThreeOfAKind(1)));
-        assertThat(Roll.roll(1,1,3,4,3).threeOfAKind()).isEmpty();
+        assertThat(
+            Roll.roll(1,1,3,1,3).threeOfAKind()
+        ).isEqualTo(
+            Optional.of(new ThreeOfAKind(1))
+        );
+
+        assertThat(
+            Roll.roll(1,1,3,4,3).threeOfAKind()
+        ).isEmpty();
     }
 
     @Test
     public void fourOfAKind() {
-        assertThat(Roll.roll(1,1,3,1,1).fourOfAKind()).isEqualTo(Optional.of(new FourOfAKind(1)));
-        assertThat(Roll.roll(1,1,1,4,3).fourOfAKind()).isEmpty();
+        assertThat(
+            Roll.roll(1,1,3,1,1).fourOfAKind()
+        ).isEqualTo(Optional.of(new FourOfAKind(1)));
+
+        assertThat(
+            Roll.roll(1,1,1,4,3).fourOfAKind()
+        ).isEmpty();
     }
 
     @Test
     public void isSmallStraight() {
-        assertThat(Roll.roll(1,2,3,4,5).isSmallStraight()).isTrue();
-        assertThat(Roll.roll(2,1,5,3,4).isSmallStraight()).isTrue();
-        assertThat(Roll.roll(2,1,5,3,2).isSmallStraight()).isFalse();
+        assertThat(
+            Roll.roll(1,2,3,4,5).isSmallStraight()
+        ).isTrue();
+
+        assertThat(
+            Roll.roll(2,1,5,3,4).isSmallStraight()
+        ).isTrue();
+
+        assertThat(
+            Roll.roll(2,1,5,3,2).isSmallStraight()
+        ).isFalse();
     }
 
     @Test
     public void isLargeStraight() {
-        assertThat(Roll.roll(2,3,4,5,6).isLargeStraight()).isTrue();
-        assertThat(Roll.roll(6,4,5,2,3).isLargeStraight()).isTrue();
-        assertThat(Roll.roll(6,4,5,2,1).isLargeStraight()).isFalse();
+        assertThat(
+            Roll.roll(2,3,4,5,6).isLargeStraight()
+        ).isTrue();
+
+        assertThat(
+            Roll.roll(6,4,5,2,3).isLargeStraight()
+        ).isTrue();
+
+        assertThat(
+            Roll.roll(6,4,5,2,1).isLargeStraight()
+        ).isFalse();
+
     }
 
     @Test
     public void isFullHouse() {
-        assertThat(Roll.roll(3,3,2,2,2).isFullHouse()).isTrue();
-        assertThat(Roll.roll(3,3,2,2,1).isFullHouse()).isFalse();
+        assertThat(
+            Roll.roll(3,3,2,2,2).isFullHouse()
+        ).isTrue();
+
+        assertThat(
+            Roll.roll(3,3,2,2,1).isFullHouse()
+        ).isFalse();
     }
 
 
--- src/test/java/fr/cla/kata/yatzy/test/CategoryTest.java
+++ src/test/java/fr/cla/kata/yatzy/test/CategoryTest.java
@@ -4,146 +4,257 @@ import fr.cla.kata.yatzy.Category;
 import fr.cla.kata.yatzy.Roll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CategoryTest {
 
     @Test
     public void chance_category_should_score_roll_to_the_sum_of_all_dice() {
-        assertEquals(15, Category.CHANCE.score(Roll.roll(2,3,4,5,1)));
-        assertEquals(16, Category.CHANCE.score(Roll.roll(3,3,4,5,1)));
+        assertThat(
+            Category.CHANCE.score(Roll.roll(2,3,4,5,1))
+        ).isEqualTo(15);
+
+        assertThat(
+            Category.CHANCE.score(Roll.roll(3,3,4,5,1))
+        ).isEqualTo(16);
+
     }
 
     @Test
     public void yatzy_category_should_score_compatible_roll_to_50() {
-        assertEquals(50, Category.YATZY.score(Roll.roll(4,4,4,4,4)));
-        assertEquals(50, Category.YATZY.score(Roll.roll(6,6,6,6,6)));
+        assertThat(
+            Category.YATZY.score(Roll.roll(4,4,4,4,4))
+        ).isEqualTo(50);
+
+        assertThat(
+            Category.YATZY.score(Roll.roll(6,6,6,6,6))
+        ).isEqualTo(50);
     }
 
     @Test
     public void yatzy_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.YATZY.score(Roll.roll(6,6,6,6,3)));
+        assertThat(
+            Category.YATZY.score(Roll.roll(6,6,6,6,3))
+        ).isEqualTo(0);
     }
 
     @Test
     public void ones_category_should_score_roll_to_the_sum_of_ones() {
-        assertEquals(1, Category.ONES.score(Roll.roll(1,2,3,4,5)));
-        assertEquals(2, Category.ONES.score(Roll.roll(1,2,1,4,5)));
-        assertEquals(4, Category.ONES.score(Roll.roll(1,2,1,1,1)));
-        assertEquals(0, Category.ONES.score(Roll.roll(6,2,2,4,5)));
+        assertThat(
+            Category.ONES.score(Roll.roll(1,2,3,4,5))
+        ).isEqualTo(1);
+
+        assertThat(
+            Category.ONES.score(Roll.roll(1,2,1,4,5))
+        ).isEqualTo(2);
+
+        assertThat(
+            Category.ONES.score(Roll.roll(1,2,1,1,1))
+        ).isEqualTo(4);
+
+        assertThat(
+            Category.ONES.score(Roll.roll(6,2,2,4,5))
+        ).isEqualTo(0);
+
     }
 
     @Test
     public void twos_category_should_score_roll_to_the_sum_of_twos() {
-        assertEquals(4, Category.TWOS.score(Roll.roll(1,2,3,2,6)));
-        assertEquals(10, Category.TWOS.score(Roll.roll(2,2,2,2,2)));
+        assertThat(
+            Category.TWOS.score(Roll.roll(1,2,3,2,6))
+        ).isEqualTo(4);
+
+        assertThat(
+            Category.TWOS.score(Roll.roll(2,2,2,2,2))
+        ).isEqualTo(10);
     }
 
     @Test
     public void threes_category_should_score_roll_to_the_sum_of_threes() {
-        assertEquals(6, Category.THREES.score(Roll.roll(1,2,3,2,3)));
-        assertEquals(12, Category.THREES.score(Roll.roll(2,3,3,3,3)));
+        assertThat(
+            Category.THREES.score(Roll.roll(1,2,3,2,3))
+        ).isEqualTo(6);
+
+        assertThat(
+            Category.THREES.score(Roll.roll(2,3,3,3,3))
+        ).isEqualTo(12);
     }
 
     @Test
     public void fours_category_should_score_roll_to_the_sum_of_fours() {
-        assertEquals(12, Category.FOURS.score(Roll.roll(4,4,4,5,5)));
-        assertEquals(8, Category.FOURS.score(Roll.roll(4,4,5,5,5)));
-        assertEquals(4, Category.FOURS.score(Roll.roll(4,5,5,5,5)));
+        assertThat(
+            Category.FOURS.score(Roll.roll(4,4,4,5,5))
+        ).isEqualTo(12);
+
+        assertThat(
+            Category.FOURS.score(Roll.roll(4,4,5,5,5))
+        ).isEqualTo(8);
+
+        assertThat(
+            Category.FOURS.score(Roll.roll(4,5,5,5,5))
+        ).isEqualTo(4);
     }
 
     @Test
     public void fives_category_should_score_roll_to_the_sum_of_fives() {
-        assertEquals(10, Category.FIVES.score(Roll.roll(4,4,4,5,5)));
-        assertEquals(15, Category.FIVES.score(Roll.roll(4,4,5,5,5)));
-        assertEquals(20, Category.FIVES.score(Roll.roll(4,5,5,5,5)));
+        assertThat(
+            Category.FIVES.score(Roll.roll(4,4,4,5,5))
+        ).isEqualTo(10);
+
+        assertThat(
+            Category.FIVES.score(Roll.roll(4,4,5,5,5))
+        ).isEqualTo(15);
+
+        assertThat(
+            Category.FIVES.score(Roll.roll(4,5,5,5,5))
+        ).isEqualTo(20);
     }
 
     @Test
     public void sixes_category_should_score_roll_to_the_sum_of_sixes() {
-        assertEquals(0, Category.SIXES.score(Roll.roll(4,4,4,5,5)));
-        assertEquals(6, Category.SIXES.score(Roll.roll(4,4,6,5,5)));
-        assertEquals(18, Category.SIXES.score(Roll.roll(6,5,6,6,5)));
+        assertThat(
+            Category.SIXES.score(Roll.roll(4,4,4,5,5))
+        ).isEqualTo(0);
+
+        assertThat(
+            Category.SIXES.score(Roll.roll(4,4,6,5,5))
+        ).isEqualTo(6);
+
+        assertThat(
+            Category.SIXES.score(Roll.roll(6,5,6,6,5))
+        ).isEqualTo(18);
     }
 
     @Test
     public void one_pair_category_should_score_compatible_roll_to_the_sum_of_the_highest_pair() {
-        assertEquals(6, Category.ONE_PAIR.score(Roll.roll(3,4,3,5,6)));
-        assertEquals(10, Category.ONE_PAIR.score(Roll.roll(5,3,3,3,5)));
-        assertEquals(12, Category.ONE_PAIR.score(Roll.roll(5,3,6,6,5)));
+        assertThat(
+            Category.ONE_PAIR.score(Roll.roll(3,4,3,5,6))
+        ).isEqualTo(6);
+
+        assertThat(
+            Category.ONE_PAIR.score(Roll.roll(5,3,3,3,5))
+        ).isEqualTo(10);
+
+        assertThat(
+            Category.ONE_PAIR.score(Roll.roll(5,3,6,6,5))
+        ).isEqualTo(12);
     }
 
     @Test
     public void one_pair_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.ONE_PAIR.score(Roll.roll(3,2,1,5,6)));
+        assertThat(
+            Category.ONE_PAIR.score(Roll.roll(3,2,1,5,6))
+        ).isEqualTo(0);
     }
 
     @Test
     public void two_pairs_category_should_score_compatible_roll_to_the_sum_of_the_two_pairs() {
-        assertEquals(16, Category.TWO_PAIRS.score(Roll.roll(3,3,5,4,5)));
-        assertEquals(16, Category.TWO_PAIRS.score(Roll.roll(3,3,5,5,5)));
+        assertThat(
+            Category.TWO_PAIRS.score(Roll.roll(3,3,5,4,5))
+        ).isEqualTo(16);
+
+        assertThat(
+            Category.TWO_PAIRS.score(Roll.roll(3,3,5,5,5))
+        ).isEqualTo(16);
     }
 
     @Test
     public void two_pairs_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.TWO_PAIRS.score(Roll.roll(3,3,1,4,5)));
+        assertThat(
+            Category.TWO_PAIRS.score(Roll.roll(3,3,1,4,5))
+        ).isEqualTo(0);
     }
 
     @Test
-    public void three_of_a_kind_category_should_score_compatible_roll_to_the_sum_of_the_three_of_a_kind()
-    {
-        assertEquals(9, Category.THREE_OF_A_KIND.score(Roll.roll(3,3,3,4,5)));
-        assertEquals(15, Category.THREE_OF_A_KIND.score(Roll.roll(5,3,5,4,5)));
-        assertEquals(9, Category.THREE_OF_A_KIND.score(Roll.roll(3,3,3,3,5)));
+    public void three_of_a_kind_category_should_score_compatible_roll_to_the_sum_of_the_three_of_a_kind() {
+        assertThat(
+            Category.THREE_OF_A_KIND.score(Roll.roll(3,3,3,4,5))
+        ).isEqualTo(9);
+
+        assertThat(
+            Category.THREE_OF_A_KIND.score(Roll.roll(5,3,5,4,5))
+        ).isEqualTo(15);
+
+        assertThat(
+            Category.THREE_OF_A_KIND.score(Roll.roll(3,3,3,3,5))
+        ).isEqualTo(9);
     }
 
     @Test
     public void three_of_a_kind_category_should_score_compatible_roll_to_0() {
-        assertEquals(0, Category.THREE_OF_A_KIND.score(Roll.roll(3,3,1,4,5)));
+        assertThat(
+            Category.THREE_OF_A_KIND.score(Roll.roll(3,3,1,4,5))
+        ).isEqualTo(0);
     }
 
     @Test
     public void four_of_a_kind_category_should_score_compatible_roll_to_the_sum_of_the_four_of_a_kind() {
-        assertEquals(12, Category.FOUR_OF_A_KIND.score(Roll.roll(3,3,3,3,5)));
-        assertEquals(20, Category.FOUR_OF_A_KIND.score(Roll.roll(5,5,5,4,5)));
-        assertEquals(12, Category.FOUR_OF_A_KIND.score(Roll.roll(3,3,3,3,3)));
+        assertThat(
+            Category.FOUR_OF_A_KIND.score(Roll.roll(3,3,3,3,5))
+        ).isEqualTo(12);
+
+        assertThat(
+            Category.FOUR_OF_A_KIND.score(Roll.roll(5,5,5,4,5))
+        ).isEqualTo(20);
+
+        assertThat(Category.FOUR_OF_A_KIND.score(Roll.roll(3,3,3,3,3))).isEqualTo(12);
     }
 
     @Test
     public void four_of_a_kind_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.FOUR_OF_A_KIND.score(Roll.roll(3,3,3,1,2)));
+        assertThat(
+            Category.FOUR_OF_A_KIND.score(Roll.roll(3,3,3,1,2))
+        ).isEqualTo(0);
     }
 
     @Test
     public void small_straight_category_should_score_compatible_roll_to_15() {
-        assertEquals(15, Category.SMALL_STRAIGHT.score(Roll.roll(1,2,3,4,5)));
-        assertEquals(15, Category.SMALL_STRAIGHT.score(Roll.roll(2,3,4,5,1)));
+        assertThat(
+            Category.SMALL_STRAIGHT.score(Roll.roll(1,2,3,4,5))
+        ).isEqualTo(15);
+
+        assertThat(
+            Category.SMALL_STRAIGHT.score(Roll.roll(2,3,4,5,1))
+        ).isEqualTo(15);
     }
 
     @Test
     public void small_straight_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.SMALL_STRAIGHT.score(Roll.roll(1,2,2,4,5)));
+        assertThat(
+            Category.SMALL_STRAIGHT.score(Roll.roll(1,2,2,4,5))
+        ).isEqualTo(0);
     }
 
     @Test
     public void large_straight_category_should_score_compatible_roll_to_20() {
-        assertEquals(20, Category.LARGE_STRAIGHT.score(Roll.roll(6,2,3,4,5)));
-        assertEquals(20, Category.LARGE_STRAIGHT.score(Roll.roll(2,3,4,5,6)));
+        assertThat(
+            Category.LARGE_STRAIGHT.score(Roll.roll(6,2,3,4,5))
+        ).isEqualTo(20);
+
+        assertThat(
+            Category.LARGE_STRAIGHT.score(Roll.roll(2,3,4,5,6))
+        ).isEqualTo(20);
     }
 
     @Test
     public void large_straight_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.LARGE_STRAIGHT.score(Roll.roll(1,2,2,4,5)));
+        assertThat(
+            Category.LARGE_STRAIGHT.score(Roll.roll(1,2,2,4,5))
+        ).isEqualTo(0);
     }
 
     @Test
     public void full_shouse_category_should_score_compatible_roll_to_the_sum_of_all_dice() {
-        assertEquals(18, Category.FULL_HOUSE.score(Roll.roll(6,2,2,2,6)));
+        assertThat(
+            Category.FULL_HOUSE.score(Roll.roll(6,2,2,2,6))
+        ).isEqualTo(18);
     }
 
     @Test
     public void full_shouse_category_should_score_incompatible_roll_to_0() {
-        assertEquals(0, Category.FULL_HOUSE.score(Roll.roll(2,3,4,5,6)));
+        assertThat(
+            Category.FULL_HOUSE.score(Roll.roll(2,3,4,5,6))
+        ).isEqualTo(0);
     }
 
 }
